5864b2503914d2da9f788f03f90f87255de76d3d
Daniel Solarte <66956234+dsolartec@users.noreply.github.com>
2022-11-13 14:41:54 -0500

    feat(tests): add grant and revoke user permissions tests

--- tests/users_service_test.go
+++ tests/users_service_test.go
@@ -349,6 +349,49 @@ func TestGetOneUser_Success(t *testing.T) {
 	}
 }
 
+func TestGetAllUserPermissions_UserNotFound(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 1}, {username: "superadmin"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnError(noResultsError)
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions", "GET", nil, accessToken)
+		if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected %d, got: %d", http.StatusBadRequest, res.StatusCode)
+		}
+
+		var errorMessage domain.ErrorMessage
+		if err := json.Unmarshal(b, &errorMessage); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		expected := "El usuario no existe"
+		if errorMessage.Message != expected {
+			t.Errorf("Expected %s, got: %s", expected, errorMessage.Message)
+		}
+	}
+}
+
 func TestGetAllUserPermissions_NoData(t *testing.T) {
 	cases := []struct {
 		ID       int
@@ -461,6 +504,492 @@ func TestGetAllUserPermissions_Success(t *testing.T) {
 		permission_test_1 := user_permissions[1].(map[string]interface{})
 		if permission_test_1["permission_name"].(string) != "permission_test_1" {
 			t.Errorf("Expected permission_test_1, got: %s", permission_test_1["permission_name"].(string))
+		}
+	}
+}
+
+func TestGrantUserPermission_UserNotFound(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 2}, {username: "meli"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"grant_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnError(noResultsError)
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "PATCH", nil, accessToken)
+		if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected %d, got: %d", http.StatusBadRequest, res.StatusCode)
+		}
+
+		var errorMessage domain.ErrorMessage
+		if err := json.Unmarshal(b, &errorMessage); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		expected := "El usuario no existe"
+		if errorMessage.Message != expected {
+			t.Errorf("Expected %s, got: %s", expected, errorMessage.Message)
+		}
+	}
+}
+
+func TestGrantUserPermission_CurrentUser(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 1}, {username: "superadmin"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"grant_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnRows(sqlmock.NewRows([]string{"id", "username", "created_at"}).AddRow(1, "superadmin", time.Now()))
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "PATCH", nil, accessToken)
+		if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected %d, got: %d", http.StatusBadRequest, res.StatusCode)
+		}
+
+		var errorMessage domain.ErrorMessage
+		if err := json.Unmarshal(b, &errorMessage); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		expected := "No puedes otorgarte un permiso a ti mismo"
+		if errorMessage.Message != expected {
+			t.Errorf("Expected %s, got: %s", expected, errorMessage.Message)
+		}
+	}
+}
+
+func TestGrantUserPermission_PermissionNotFound(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 2}, {username: "meli"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"grant_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnRows(sqlmock.NewRows([]string{"id", "username", "created_at"}).AddRow(2, "meli", time.Now()))
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, name, description, deletable, editable, created_at, updated_at FROM permissions WHERE name = $1;")).
+			WithArgs("permission_test").
+			WillReturnError(noResultsError)
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "PATCH", nil, accessToken)
+		if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected %d, got: %d", http.StatusBadRequest, res.StatusCode)
+		}
+
+		var errorMessage domain.ErrorMessage
+		if err := json.Unmarshal(b, &errorMessage); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		expected := "El permiso no existe"
+		if errorMessage.Message != expected {
+			t.Errorf("Expected %s, got: %s", expected, errorMessage.Message)
+		}
+	}
+}
+
+func TestGrantUserPermission_AlreadyHasPermission(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 2}, {username: "meli"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"grant_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnRows(sqlmock.NewRows([]string{"id", "username", "created_at"}).AddRow(2, "meli", time.Now()))
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, name, description, deletable, editable, created_at, updated_at FROM permissions WHERE name = $1;")).
+			WithArgs("permission_test").
+			WillReturnRows(
+				sqlmock.NewRows([]string{"id", "name", "description", "deletable", "editable", "created_at", "updated_at"}).
+					AddRow(1, "permission_test", "Este es un permiso de prueba", false, false, time.Now(), time.Now()),
+			)
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, user_id, permission_id FROM user_permissions WHERE user_id = $1 AND permission_id = $2;")).
+			WithArgs(2, 1).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "user_id", "permission_id"}).AddRow(1, 2, 1))
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "PATCH", nil, accessToken)
+		if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected %d, got: %d", http.StatusBadRequest, res.StatusCode)
+		}
+
+		var errorMessage domain.ErrorMessage
+		if err := json.Unmarshal(b, &errorMessage); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		expected := "El usuario ya tiene el permiso asignado"
+		if errorMessage.Message != expected {
+			t.Errorf("Expected %s, got: %s", expected, errorMessage.Message)
+		}
+	}
+}
+
+func TestGrantUserPermission_Success(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 2}, {username: "meli"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"grant_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnRows(sqlmock.NewRows([]string{"id", "username", "created_at"}).AddRow(2, "meli", time.Now()))
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, name, description, deletable, editable, created_at, updated_at FROM permissions WHERE name = $1;")).
+			WithArgs("permission_test").
+			WillReturnRows(
+				sqlmock.NewRows([]string{"id", "name", "description", "deletable", "editable", "created_at", "updated_at"}).
+					AddRow(1, "permission_test", "Este es un permiso de prueba", false, false, time.Now(), time.Now()),
+			)
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, user_id, permission_id FROM user_permissions WHERE user_id = $1 AND permission_id = $2;")).
+			WithArgs(2, 1).
+			WillReturnError(noResultsError)
+
+		mock.ExpectQuery(regexp.QuoteMeta("INSERT INTO user_permissions (user_id, permission_id) VALUES ($1, $2) RETURNING id;")).
+			WithArgs(2, 1).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "PATCH", nil, accessToken)
+		if res.StatusCode != http.StatusOK {
+			t.Errorf("Expected %d, got: %d", http.StatusOK, res.StatusCode)
+		}
+
+		location := res.Header.Get("Location")
+		if location != "/api/users/"+find+"/permissions/permission_test1" {
+			t.Errorf("Expected /api/users/%s/permissions/permission_test1, got: %s", find, location)
+		}
+
+		var data domain.Map
+		if err := json.Unmarshal(b, &data); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		user_permission := data["user_permission"].(map[string]interface{})
+		if user_permission["id"].(float64) != 1 {
+			t.Errorf("Expected 1, got: %f", user_permission["id"].(float64))
+		}
+	}
+}
+
+func TestRevokeUserPermission_UserNotFound(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 2}, {username: "meli"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"revoke_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnError(noResultsError)
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "DELETE", nil, accessToken)
+		if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected %d, got: %d", http.StatusBadRequest, res.StatusCode)
+		}
+
+		var errorMessage domain.ErrorMessage
+		if err := json.Unmarshal(b, &errorMessage); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		expected := "El usuario no existe"
+		if errorMessage.Message != expected {
+			t.Errorf("Expected %s, got: %s", expected, errorMessage.Message)
+		}
+	}
+}
+
+func TestRevokeUserPermission_CurrentUser(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 1}, {username: "superadmin"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"revoke_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnRows(sqlmock.NewRows([]string{"id", "username", "created_at"}).AddRow(1, "superadmin", time.Now()))
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "DELETE", nil, accessToken)
+		if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected %d, got: %d", http.StatusBadRequest, res.StatusCode)
+		}
+
+		var errorMessage domain.ErrorMessage
+		if err := json.Unmarshal(b, &errorMessage); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		expected := "No puedes quitarte un permiso a ti mismo"
+		if errorMessage.Message != expected {
+			t.Errorf("Expected %s, got: %s", expected, errorMessage.Message)
+		}
+	}
+}
+
+func TestRevokeUserPermission_PermissionNotFound(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 2}, {username: "meli"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"revoke_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnRows(sqlmock.NewRows([]string{"id", "username", "created_at"}).AddRow(2, "meli", time.Now()))
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, name, description, deletable, editable, created_at, updated_at FROM permissions WHERE name = $1;")).
+			WithArgs("permission_test").
+			WillReturnError(noResultsError)
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "DELETE", nil, accessToken)
+		if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected %d, got: %d", http.StatusBadRequest, res.StatusCode)
+		}
+
+		var errorMessage domain.ErrorMessage
+		if err := json.Unmarshal(b, &errorMessage); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		expected := "El permiso no existe"
+		if errorMessage.Message != expected {
+			t.Errorf("Expected %s, got: %s", expected, errorMessage.Message)
+		}
+	}
+}
+
+func TestRevokeUserPermission_NoHasPermission(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 2}, {username: "meli"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"revoke_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnRows(sqlmock.NewRows([]string{"id", "username", "created_at"}).AddRow(2, "meli", time.Now()))
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, name, description, deletable, editable, created_at, updated_at FROM permissions WHERE name = $1;")).
+			WithArgs("permission_test").
+			WillReturnRows(
+				sqlmock.NewRows([]string{"id", "name", "description", "deletable", "editable", "created_at", "updated_at"}).
+					AddRow(1, "permission_test", "Este es un permiso de prueba", false, false, time.Now(), time.Now()),
+			)
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, user_id, permission_id FROM user_permissions WHERE user_id = $1 AND permission_id = $2;")).
+			WithArgs(2, 1).
+			WillReturnError(noResultsError)
+
+		res, b := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "DELETE", nil, accessToken)
+		if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("Expected %d, got: %d", http.StatusBadRequest, res.StatusCode)
+		}
+
+		var errorMessage domain.ErrorMessage
+		if err := json.Unmarshal(b, &errorMessage); err != nil {
+			t.Fatalf("Could not unmarshall response %v", err)
+		}
+
+		expected := "El usuario no tiene este permiso asignado"
+		if errorMessage.Message != expected {
+			t.Errorf("Expected %s, got: %s", expected, errorMessage.Message)
+		}
+	}
+}
+
+func TestRevokeUserPermission_Success(t *testing.T) {
+	cases := []struct {
+		ID       int
+		username string
+	}{{ID: 2}, {username: "meli"}}
+
+	for _, td := range cases {
+		serv, mock := newTestServer()
+
+		accessToken := generateAccessToken(t, mock, []string{"revoke_permission"})
+
+		var (
+			query *sqlmock.ExpectedQuery
+			find  string
+		)
+
+		if td.username != "" {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE username = $1;")).WithArgs(td.username)
+			find = td.username
+		} else {
+			query = mock.ExpectQuery(regexp.QuoteMeta("SELECT id, username, created_at FROM users WHERE id = $1;")).WithArgs(td.ID)
+			find = fmt.Sprint(td.ID)
+		}
+
+		query.WillReturnRows(sqlmock.NewRows([]string{"id", "username", "created_at"}).AddRow(2, "meli", time.Now()))
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, name, description, deletable, editable, created_at, updated_at FROM permissions WHERE name = $1;")).
+			WithArgs("permission_test").
+			WillReturnRows(
+				sqlmock.NewRows([]string{"id", "name", "description", "deletable", "editable", "created_at", "updated_at"}).
+					AddRow(1, "permission_test", "Este es un permiso de prueba", false, false, time.Now(), time.Now()),
+			)
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT id, user_id, permission_id FROM user_permissions WHERE user_id = $1 AND permission_id = $2;")).
+			WithArgs(2, 1).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "user_id", "permission_id"}).AddRow(1, 2, 1))
+
+		mock.ExpectPrepare(regexp.QuoteMeta("DELETE FROM user_permissions WHERE id = $1;")).
+			ExpectExec().
+			WithArgs(1).
+			WillReturnResult(sqlmock.NewResult(0, 1))
+
+		res, _ := request(t, serv, "/api/users/"+find+"/permissions/permission_test", "DELETE", nil, accessToken)
+		if res.StatusCode != http.StatusOK {
+			t.Errorf("Expected %d, got: %d", http.StatusOK, res.StatusCode)
 		}
 	}
 }
